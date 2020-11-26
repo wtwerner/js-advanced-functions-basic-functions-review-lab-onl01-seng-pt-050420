5aaa171a2e7eed4bdf3e550f841c9cd6c5381761
wtwerner <wtwerner@gmail.com>
2020-11-26 13:34:32 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Your code here
+function saturdayFun(str = "roller-skate") {
+    return `This Saturday, I want to ${str}!`
+}
+
+function mondayWork(str = "go to the office") {
+    return `This Monday, I will ${str}.`
+}
+
+function wrapAdjective(sym = "*") {
+    return function innerFunction(adj = "special") {
+        return `You are ${sym+adj+sym}!`
+    }
+}
+
+let Calculator = {
+    add: function(num1, num2) {
+        return num1 + num2
+    },
+    subtract: function(num1, num2) {
+        return num1 - num2
+    },
+    multiply: function(num1, num2) {
+        return num1 * num2
+    },
+    divide: function(num1, num2) {
+        return num1 / num2
+    }
+}
+
+function actionApplyer(start, arr) {
+    let int = start
+    arr.forEach(f => {
+        int = f(int)
+    })
+    return int
+}
